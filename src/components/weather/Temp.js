825fbd574408630ce2e71431a701847572b882b1
shahinurdev <mdshahinur6@gmail.com>
2021-08-13 22:50:57 +0600

    bugfixe

--- src/components/weather/Temp.js
+++ src/components/weather/Temp.js
@@ -4,20 +4,20 @@ import WeatherCard from './WeatherCard';
 const Temp = () => {
     const [searchValue, setSearchValue] = useState('jamalpur');
     const [tempInfo, setTempInfo] = useState({})
-    const getWeatherInfo = async () =>{
+    const getWeatherInfo = async () => {
         try {
-            let url = `http://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=d05788f3e04125c4f961761782c09018`;
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=d05788f3e04125c4f961761782c09018`;
             const res = await fetch(url);
             const data = await res.json();
-            const {temp, humidity, pressure} = data.main;
-            const {main: weathermood} = data.weather[0];
-            const {name} = data;
-            const {speed} = data.wind;
-            const {country, sunset} = data.sys;
+            const { temp, humidity, pressure } = data.main;
+            const { main: weathermood } = data.weather[0];
+            const { name } = data;
+            const { speed } = data.wind;
+            const { country, sunset } = data.sys;
 
             const myNewWeather = {
-                temp, 
-                humidity, 
+                temp,
+                humidity,
                 pressure,
                 weathermood,
                 name,
@@ -25,29 +25,28 @@ const Temp = () => {
                 country,
                 sunset
             };
-               setTempInfo(myNewWeather)
-            console.log(temp);
+            setTempInfo(myNewWeather)
         } catch (error) {
             console.log(error);
         }
     };
     useEffect(() => {
         getWeatherInfo();
-    }, [])
-     
+    })
+
     return (
         <>
-        <br /><br />
-           <div className="wrap">
-               <div className="search">
-                   <input type="search" placeholder="Search" autoFocus id="search"
-                    value={searchValue}
-                    onChange={(e)=>setSearchValue(e.target.value)}
-                   className="searchTerm" />
-                   <button className="searchButton" type="button" onClick={getWeatherInfo}>Search </button>
-               </div>
-            </div> 
-           <WeatherCard tempInfo={tempInfo}/>
+            <br /><br />
+            <div className="wrap">
+                <div className="search">
+                    <input type="search" placeholder="Search" autoFocus id="search"
+                        value={searchValue}
+                        onChange={(e) => setSearchValue(e.target.value)}
+                        className="searchTerm" />
+                    <button className="searchButton" type="button" onClick={getWeatherInfo}>Search </button>
+                </div>
+            </div>
+            <WeatherCard tempInfo={tempInfo} />
         </>
     );
 };
